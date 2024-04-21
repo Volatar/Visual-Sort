50c38aa953fc7db149b60c99d584810199e3b5bb
JMoukas <144281540+JMoukas@users.noreply.github.com>
2024-04-21 15:04:30 -0400

    Added the bubble sort method to the project

--- bubbleSort.py
+++ bubbleSort.py
@@ -2,10 +2,30 @@ from dataStructures import Array
 
 
 def bubbleSort(array):
-    # written by ____
+    # written by John Moukas
     # implement bubble sort
     # output the contents of the array using the __str__ method each step of the way to show your work
-    # return the sorted array
-    pass
+    arrayLength = len(array)
+    while arrayLength > 1:
+        swapped = False
+        i = 1
+        while i < arrayLength:
+            if array[i] < array[i - 1]:
+                # Swap variables
+                array[i], array[i - 1] = array[i - 1], array[i]
+                swapped = True
+            i += 1
+        print(str(array))
+        if not swapped: return
+        arrayLength -= 1
+        
+    return array
+
+#Testing: To be removed later
+if __name__ == "__main__":
+    array = [4, 6, 2, 3, 1, 0]
+    sorted_arr = bubbleSort(array)
+    print(sorted_arr)
+    
 
 
